48a1bf43521f545e84b6ff0ab297b627f2737f89
Laurent <laurent.hiernard@xam.com.au>
2022-01-25 12:11:17 +1100

    header content

--- React/library/src/components/Layout.tsx
+++ React/library/src/components/Layout.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import Button from "./../components/Button";
 import ButtonGroup from "./../components/ButtonGroup";
 import Header from "./Header";
 import { ReactComponent as Logo } from "./../assets/logo.svg";
+import { ReactComponent as IconProfile } from "./../assets/icons/icon-profile.svg";
+
+const HeaderContent = styled.div`
+  display: flex;
+  flex: 1 0 auto;
+  justify-content: flex-end;
+  & > :nth-child(1) {
+    margin-right: 36px;
+  }
+  & > :nth-child(2) {
+    flex: 0 1 220px;
+  }
+`;
 
 type ILayoutProps = {
   children: React.ReactNode;
@@ -14,19 +28,30 @@ const Layout = ({ children }: ILayoutProps) => {
   return (
     <>
       <Header title="React Components Showcase" logo={<Logo />}>
-        <nav>
-          <ButtonGroup inline>
-            <Link to="/">
-              <Button label="Home" color="primary"></Button>
-            </Link>
-            <Link to="/news">
-              <Button label="What’s New" color="primary"></Button>
-            </Link>
-            <Link to="/profile">
-              <Button label="My Profile" color="primary"></Button>
-            </Link>
-          </ButtonGroup>
-        </nav>
+        <HeaderContent>
+          {/* NAVIGATION */}
+          <nav>
+            <ButtonGroup inline>
+              <Link to="/">
+                <Button label="Home" color="primary"></Button>
+              </Link>
+              <Link to="/news">
+                <Button label="What’s New" color="primary"></Button>
+              </Link>
+              <Link to="/profile">
+                <Button label="My Profile" color="primary"></Button>
+              </Link>
+            </ButtonGroup>
+          </nav>
+          {/* LOGIN */}
+          <Button
+            label="Login"
+            color="primary"
+            variant="outlined"
+            wide
+            startIcon={<IconProfile />}
+          />
+        </HeaderContent>
       </Header>
       <main>{children}</main>
     </>
